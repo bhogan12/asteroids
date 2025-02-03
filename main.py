387677f7b762a99831f6f93c5ee6f13d1bb31339
bhogan12 <benhogan12@gmail.com>
2025-02-03 15:18:10 -0500

    player controls, asteroid spawns, collision detection, shoot to delete asteroids

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
@@ -9,6 +11,18 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    
+    Shot.containers = (shots, drawable, updatable)
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    afield = AsteroidField()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -16,10 +30,24 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        screen.fill("black")
-        player.draw(screen)
-        pygame.display.flip()
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.check_collision(player):
+                print("Game over!")
+                return
         
+            for shot in shots:
+                if asteroid.check_collision(shot):
+                    asteroid.kill()
+                    shot.kill()
+
+        screen.fill("black")
+
+        for object in drawable:
+            object.draw(screen)
+        
+        pygame.display.flip()
 
         dt = clock.tick(60) / 1000
 
